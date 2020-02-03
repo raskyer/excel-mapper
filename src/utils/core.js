e5296abbd7b249f96dbba8dc57e26cc7f40fa745
vrasquier <vincent.rasquier@rakuten.com>
2020-02-03 18:25:14 +0100

    Better get ranking

--- src/utils/core.js
+++ src/utils/core.js
@@ -44,88 +44,13 @@ function createMap(sheet, idCell) {
   return map;
 }
 
-function calculateProvider(provider, settings) {
-  if (provider === undefined || typeof provider[settings.providerRating] !== 'number') {
-    console.warn('CALCULATE PROVIDER : provider undefined or invalide rating', provider, settings.providerRating);
-    return 1;
-  }
-
-  if (provider[settings.providerRating] > 5 || provider[settings.providerRating] < 0) {
-    console.warn('CALCULATE PROVIDER : invalide provider mark range', provider[settings.providerRating]);
-  } 
-
-  const mark = 6 - provider[settings.providerRating];
-  const coef = typeof settings.providerRate === 'number' ? settings.providerRate : 1;
-
-  return mark * coef;
-}
-
-function caclulateDate(date, settings) {
-  if (date === undefined || typeof date !== 'object') {
-    console.log('CALCULATE DATE : date undefined or invalide date', date);
-    return 1;
-  }
-
-  const rate = 24 - date.getHours();
-  const mark = (rate / 24) * 5
-  const coef = typeof settings.dateRate === 'number' ? settings.dateRate : 1;
-
-  return mark * coef;
-}
-
-function calculateCustomer(customer, settings) {
-  if (customer === undefined || typeof customer[settings.customerRating] !== 'string') {
-    console.warn('CALCULATE CUSTOMER : customer undefined or invalid rating', customer, settings.customerRating);
-    return 1;
-  }
-  let mark = 1;
-  const coef = typeof settings.customerRate === 'number' ? settings.customerRate : 1;
-  const rating = customer[settings.customerRating].toLowerCase().trim();
-  switch (rating) {
-    case 'sensible':
-      mark = 5;
-      break;
-    default:
-      console.warn('CALCULATE CUSTOMER : unchecked rating', rating);
-      mark = 1;
-      break;
-  }
-  return mark * coef;
-}
-
 function createOrderRanking(customerMap, providerMap, orderSheet, settings) {
   const orders = [];
   for (let i = 1; i < orderSheet.length; i++) {
-    let customerKey = orderSheet[i][settings.orderCustomerID];
-    let providerKey = orderSheet[i][settings.orderProviderID];
-
-    if (typeof customerKey === 'string') {
-      customerKey = customerKey.trim().toLowerCase();
-    }
-
-    if (typeof providerKey === 'string') {
-      providerKey = providerKey.trim().toLowerCase();
-    }
-
-    const customer = customerMap.get(customerKey);
-    const provider = providerMap.get(providerKey);
-    const date     = orderSheet[i][settings.orderType].trim().toLowerCase() === 'chargement' ?
-      orderSheet[i][settings.orderDateShipping] : orderSheet[i][settings.orderDateDelivery];
-
-    if (provider === undefined) {
-      console.log('CREATE ORDER RANKING : undefined provider', providerKey);
-    }
-    if (customer === undefined) {
-      console.log('CREATE ORDER RANKING : undefined customer', customerKey);
-    }
-
-    const providerRanking = calculateProvider(provider, settings);
-    const dateRanking     = caclulateDate(date, settings);
-    const customerRanking = calculateCustomer(customer, settings);
-
-    const ranking = providerRanking + dateRanking + customerRanking;
+    const o = orderSheet[i];
+    const ranking = getRanking(o, customerMap, providerMap, settings);
     
-    orders.push({ order: orderSheet[i], ranking })
+    orders.push({ order: o, ranking })
   }
   return orders.sort((a, b) => {
     if (a.order[settings.orderType] !== b.order[settings.orderType]) {
@@ -133,6 +58,133 @@ function createOrderRanking(customerMap, providerMap, orderSheet, settings) {
     }
     return b.ranking - a.ranking
   });
+}
+
+function getRanking(o, customerMap, providerMap, settings) {
+  const customerRanking = getCustomerRanking(o[settings.orderCustomerID], customerMap, settings);
+  const providerRanking = getProviderRanking(o[settings.orderProviderID], providerMap, settings);
+  const dateRanking     = getDateRanking(o, settings);
+
+  return providerRanking + dateRanking + customerRanking;
+}
+
+function getCustomerRanking(customerKey, customerMap, settings) {
+  if (customerKey === undefined || customerKey === null || customerKey === '') {
+    console.error('GET CUSTOMER RANKING : customer key is empty', customerKey);
+    return 0;
+  }
+
+  if (typeof customerKey !== 'string' && typeof customerKey !== 'number') {
+    console.error('GET CUSTOMER RANKING : customer key is of invalid type', typeof customerKey);
+    return 0;
+  }
+
+  if (typeof customerKey === 'string') {
+    customerKey = customerKey.trim().toLowerCase();
+  }
+
+  const customer = customerMap.get(customerKey);
+
+  if (customer === undefined) {
+    console.warn('GET CUSTOMER RANKING : unknown key in customer map', customerKey, customerMap);
+    return 0;
+  }
+
+  if (typeof customer[settings.customerRating] !== 'string') {
+    console.warn('GET CUSTOMER RANKING : customer rating is not a string', typeof customer[settings.customerRating]);
+    return 0;
+  }
+
+  if (typeof settings.customerRate !== 'number') {
+    console.warn('GET CUSTOMER RANKING : customer rate is not a number', typeof settings.customerRate);
+  }
+
+  const coef = typeof settings.customerRate === 'number' ? settings.customerRate : 1;
+  const rating = customer[settings.customerRating].toLowerCase().trim();
+  switch (rating) {
+    case 'sensible':
+      return 5 * coef;
+    default:
+      return 1 * coef;
+  }
+}
+
+function getProviderRanking(providerKey, providerMap, settings) {
+  if (providerKey === undefined || providerKey === null || providerKey === '') {
+    console.error('GET PROVIDER RANKING : provider key is empty', providerKey);
+    return 0;
+  }
+
+  if (typeof providerKey !== 'string' && typeof providerKey !== 'number') {
+    console.error('GET PROVIDER RANKING : provider key is of invalid type', typeof providerKey);
+    return 0;
+  }
+
+  if (typeof providerKey === 'string') {
+    providerKey = providerKey.trim().toLowerCase();
+  }
+
+  const provider = providerMap.get(providerKey);
+
+  if (provider === undefined) {
+    console.warn('GET PROVIDER RANKING : unknown key in provider map', providerKey, providerMap);
+    return 0;
+  }
+
+  const providerRating = provider[settings.providerRating];
+
+  if (typeof providerRating !== 'number') {
+    console.warn('GET PROVIDER RANKING : provider rating is not a number', typeof providerRating);
+    return 0;
+  }
+
+  if (providerRating > 5 || providerRating < 0) {
+    console.warn('GET PROVIDER RANKING : invalid provider mark range', providerRating);
+  }
+
+  if (typeof settings.providerRate !== 'number') {
+    console.warn('GET PROVIDER RANKING : provider rate is not a number', typeof settings.providerRate);
+  }
+
+  const mark = 6 - providerRating;
+  const coef = typeof settings.providerRate === 'number' ? settings.providerRate : 1;
+
+  return mark * coef;
+}
+
+function getDateRanking(o, settings) {
+  const type = o[settings.orderType];
+
+  if (type === undefined || type === null || type === '') {
+    console.error('GET DATE RANKING : type is empty', o, settings.orderType);
+    return 0;
+  }
+
+  if (typeof type !== 'string') {
+    console.error('GET DATE RANKING : type is not a string', typeof type);
+    return 0;
+  }
+
+  const trimType = type.trim().toLowerCase();
+
+  if (trimType !== 'chargement' && trimType !== 'livraison') {
+    console.error('GET DATE RANKING : type is not valid, await "chargement" or "livraison"', trimType);
+    return 0;
+  }
+
+  const date = trimType === 'chargement' ?
+    o[settings.orderDateShipping] : o[settings.orderDateDelivery];
+
+  if (date === undefined || typeof date !== 'object') {
+    console.log('GET DATE RANKING : date undefined or invalid', date);
+    return 0;
+  }
+
+  const rate = 24 - date.getHours();
+  const mark = (rate / 24) * 5
+  const coef = typeof settings.dateRate === 'number' ? settings.dateRate : 1;
+
+  return mark * coef;
 }
 
 function createProjection(orders, headers, projection) {
